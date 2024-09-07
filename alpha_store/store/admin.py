39a0f8674a29b5d77b3eeaecd43f3b4e212ea6ef
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-09-07 18:46:03 +0300

    feat: add base admin configuration for main models

--- alpha_store/store/admin.py
+++ alpha_store/store/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Product, Category, SubCategory
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 1
+    readonly_fields = (
+        "name",
+        "price",
+        "category",
+        "subcategory",
+        "thumbnail",
+        "medium_image",
+        "large_image",
+        "slug",
+    )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category", "subcategory", "thumbnail",)
+    list_display_links = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "image",)
+    # list_editable = ("name", "slug",)
+    inlines = (ProductInline,)
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "category",)
+    # list_editable = ("name", "slug",)
