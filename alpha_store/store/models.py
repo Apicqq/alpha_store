e6b30c00175ea150d1fd84a6f05859d8d4fb80be
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-09-07 18:04:13 +0300

    feat: add core app

--- alpha_store/store/models.py
+++ alpha_store/store/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+class Category(models.Model):
+    pass
+
+
+class SubCategory(models.Model):
+    pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    subcategories = models.ManyToManyField(SubCategory, related_name="products")
+    thumbnail = models.ImageField(upload_to="images/thumbnails")
+    medium_image = models.ImageField(upload_to="images/medium")
+    large_image = models.ImageField(upload_to="images/large")
+
+    def __str__(self):
+        return self.name[:30]
+
+
+class Cart(models.Model):
+    pass
