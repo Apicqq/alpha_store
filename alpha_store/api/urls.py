7f6cf0a5eec53dc8b183f2591b4b8bbc2c1bc859
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-09-08 08:11:46 +0300

    feat: add url pathing

---
+++ alpha_store/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import ProductViewSet, ShoppingCartViewSet
+
+v1_router = DefaultRouter()
+
+v1_router.register("products", ProductViewSet, "products")
+v1_router.register("shopping_cart", ShoppingCartViewSet, "shopping_cart")
+
+urlpatterns = [
+    path('', include(v1_router.urls)),
+]
